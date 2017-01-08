aab340e17ebf566d8c70caa7eb0728fec5b050d0
Duane Murphy <duanemurphy@mac.com>
2017-01-08 10:26:48 -0800

    Fix initialization order
    Initialization order must be the same as declaration order.

--- src/Subsystems/Chassis.cpp
+++ src/Subsystems/Chassis.cpp
@@ -4,12 +4,12 @@
 #include "../Commands/TankDrive.h"
 
 Chassis::Chassis() : Subsystem("Chassis"),
-left1Wheel(RobotMap::chassisLeft1Wheel),
-left2Wheel(RobotMap::chassisLeft2Wheel),
-left3Wheel(RobotMap::chassisLeft3Wheel),
 right1Wheel(RobotMap::chassisRight1Wheel),
 right2Wheel(RobotMap::chassisRight2Wheel),
-right3Wheel(RobotMap::chassisRight3Wheel) {
+right3Wheel(RobotMap::chassisRight3Wheel),
+left1Wheel(RobotMap::chassisLeft1Wheel),
+left2Wheel(RobotMap::chassisLeft2Wheel),
+left3Wheel(RobotMap::chassisLeft3Wheel) {
 
     right1Wheel->SetInverted(true);
 
@@ -28,11 +28,11 @@ void Chassis::InitDefaultCommand() {
 // here. Call these from Commands.
 
 void Chassis::SetTankDrive(double left, double right) {
-  left1Wheel->Set(left);
-  left2Wheel->Set(left);
-  left3Wheel->Set(left);
-
   right1Wheel->Set(right);
   right2Wheel->Set(right);
   right3Wheel->Set(right);
+
+  left1Wheel->Set(left);
+  left2Wheel->Set(left);
+  left3Wheel->Set(left);
 }
