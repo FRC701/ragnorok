0930e48306752489ac1e9660eb9332443b2aaef5
Mr. Murphy <dmurphy@vandenrobotics.com>
2017-01-08 11:39:53 -0800

    Chassis: Support followers
    Establish that the left and right motors should follow one of the motors.
    Use IDs for the motors.

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -6,7 +6,15 @@
 class RobotMap {
 public:
 
-	static std::shared_ptr<CANTalon> chassisRight1Wheel;
+  static const int kIDRightWheel1 = 10;
+  static const int kIDRightWheel2 = 11;
+  static const int kIDRightWheel3 = 12;
+  static const int kIDLeftWheel1 = 13;
+  static const int kIDLeftWheel2 = 14;
+  static const int kIDLeftWheel3 = 15;
+
+
+  static std::shared_ptr<CANTalon> chassisRight1Wheel;
 	static std::shared_ptr<CANTalon> chassisRight2Wheel;
 	static std::shared_ptr<CANTalon> chassisRight3Wheel;
 	static std::shared_ptr<CANTalon> chassisLeft1Wheel;
--- src/Subsystems/Chassis.cpp
+++ src/Subsystems/Chassis.cpp
@@ -4,23 +4,31 @@
 #include "../Commands/TankDrive.h"
 
 Chassis::Chassis() : Subsystem("Chassis"),
-right1Wheel(RobotMap::chassisRight1Wheel),
-right2Wheel(RobotMap::chassisRight2Wheel),
-right3Wheel(RobotMap::chassisRight3Wheel),
-left1Wheel(RobotMap::chassisLeft1Wheel),
-left2Wheel(RobotMap::chassisLeft2Wheel),
-left3Wheel(RobotMap::chassisLeft3Wheel) {
+  right1Wheel(RobotMap::chassisRight1Wheel),
+  right2Wheel(RobotMap::chassisRight2Wheel),
+  right3Wheel(RobotMap::chassisRight3Wheel),
+  left1Wheel(RobotMap::chassisLeft1Wheel),
+  left2Wheel(RobotMap::chassisLeft2Wheel),
+  left3Wheel(RobotMap::chassisLeft3Wheel) {
 
-    right1Wheel->SetInverted(true);
+  right1Wheel->SetInverted(true);
 
-    //TODO:look into follower mode's changes
+  right2Wheel->SetControlMode(CANTalon::kFollower);
+  right2Wheel->Set(RobotMap::kIDRightWheel1);
+  right3Wheel->SetControlMode(CANTalon::kFollower);
+  right3Wheel->Set(RobotMap::kIDRightWheel1);
+
+  left2Wheel->SetControlMode(CANTalon::kFollower);
+  left2Wheel->Set(RobotMap::kIDLeftWheel1);
+  left3Wheel->SetControlMode(CANTalon::kFollower);
+  left3Wheel->Set(RobotMap::kIDLeftWheel1);
 }
 
 void Chassis::InitDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // SetDefaultCommand(new MySpecialCommand());
+  // Set the default command for a subsystem here.
+  // SetDefaultCommand(new MySpecialCommand());
 
-        SetDefaultCommand(new TankDrive());
+  SetDefaultCommand(new TankDrive());
 }
 
 
@@ -29,10 +37,5 @@ void Chassis::InitDefaultCommand() {
 
 void Chassis::SetTankDrive(double left, double right) {
   right1Wheel->Set(right);
-  right2Wheel->Set(right);
-  right3Wheel->Set(right);
-
   left1Wheel->Set(left);
-  left2Wheel->Set(left);
-  left3Wheel->Set(left);
 }
