77af9a81dcf1e8d1deb3908adf3c66c59cda73bf
SkylerPIlot <SkylerParthemore@gmail.com>
2017-01-09 15:59:32 -0800

    Made theoretical motors in Subsystem and two functions for commands

--- src/Subsystems/Shooter.cpp
+++ src/Subsystems/Shooter.cpp
@@ -1,6 +1,7 @@
 #include "Shooter.h"
 #include "../RobotMap.h"
 #include "CANTalon.h"
+#include "Commands/DefaultShooter.h"
 
 const char Shooter::kSubsystemName[] = "Shooter";
 
@@ -13,14 +14,30 @@ std::shared_ptr<Shooter> Shooter::getInstance() {
 	return self;
 }
 
-Shooter::Shooter() : Subsystem(kSubsystemName) {
+Shooter::Shooter() : Subsystem(kSubsystemName),
+
+	kID1(RobotMap::kID1),
+	kID2(RobotMap::kID2){
+
+	kID2.SetInverted(true);
 
 }
 
 void Shooter::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	// SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new DefaultShooter());
 }
 
+void Shooter::SetShooter(double speed){
+
+	kID1.Set(speed);
+	kID2.Set(speed);
+}
+
+double Shooter::GetShooterspeed(){
+
+	return kID1.Get();
+}
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
