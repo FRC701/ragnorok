92e55b85105ba67ff6354718675f17a0038a34fe
SkylerPIlot <SkylerParthemore@gmail.com>
2017-01-10 11:59:26 -0800

    Remade the command using the new Instant Command
    Fixed code requested by Olivia.

--- src/Commands/SetShooter.h
+++ src/Commands/SetShooter.h
@@ -1,16 +1,12 @@
 #ifndef SetShooter_H
 #define SetShooter_H
 
-#include "Commands/Command.h"
+#include "Commands/InstantCommand.h"
 
-class SetShooter : public frc::Command {
+class SetShooter : public frc::InstantCommand {
 public:
 	SetShooter(double speed);
 	void Initialize();
-	void Execute();
-	bool IsFinished();
-	void End();
-	void Interrupted();
 private:
 	double mSpeed;
 };
