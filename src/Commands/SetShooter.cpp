e9e2efefe235425b3bb8f754ff909f099c5ad709
SkylerPIlot <SkylerParthemore@gmail.com>
2017-01-10 11:59:26 -0800

    Remade the command using the new Instant Command
    Fixed code requested by Olivia.

--- src/Commands/SetShooter.cpp
+++ src/Commands/SetShooter.cpp
@@ -1,36 +1,13 @@
 #include "SetShooter.h"
-#include "../Subsystems/Shooter.h"
 std::shared_ptr<Shooter> shooter = Shooter::getInstance();
 SetShooter::SetShooter(double speed) : Command(),
 		mSpeed(speed) {
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(Robot::chassis.get());
 	Requires(Shooter::getInstance().get());
-
 }
 
-// Called just before this Command runs the first time
+// Called once when the command executes
 void SetShooter::Initialize() {
-
-}
-
-// Called repeatedly when this Command is scheduled to run
-void SetShooter::Execute() {
 	shooter->SetShooter(mSpeed);
-}
-
-// Make this return true when this Command no longer needs to run execute()
-bool SetShooter::IsFinished() {
-	return false;
-}
-
-// Called once after isFinished returns true
-void SetShooter::End() {
-
-}
-
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
-void SetShooter::Interrupted() {
-
 }
