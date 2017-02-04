b8e2a7d643677fe243b75bbac2e548e2a299c031
SkylerPIlot <SkylerParthemore@gmail.com>
2017-02-04 12:30:50 -0800

    Made a SetLifter Command

---
+++ src/Commands/SetLifter.cpp
@@ -0,0 +1,35 @@
+#include "SetLifter.h"
+#include "../Subsystems/Lifter.h"
+
+SetLifter::SetLifter(double speed) :
+		mSpeed(speed)	{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Lifter::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void SetLifter::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SetLifter::Execute() {
+	Lifter::getInstance()->SetLifter(mSpeed);
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool SetLifter::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void SetLifter::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void SetLifter::Interrupted() {
+
+}
