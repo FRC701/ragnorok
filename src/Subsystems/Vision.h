49d9767e4fbac2d8232de202ea5dff60a8267a79
Mr. Murphy <dmurphy@vandenrobotics.com>
2017-02-06 18:24:03 -0800

    Vision: Detab and Private Constructor
    Detab vision.cpp and vision.h
    Make the constructor private.

--- src/Subsystems/Vision.h
+++ src/Subsystems/Vision.h
@@ -1,21 +1,20 @@
 #ifndef Vision_H
 #define Vision_H
 #include <Commands/Subsystem.h>
-#include <CANTalon.h>
 
 class Vision : public frc::Subsystem {
 private:
-	// It's desirable that everything possible under private except
-	// for methods that implement subsystem capabilities
+  // It's desirable that everything possible under private except
+  // for methods that implement subsystem capabilities
 
-	static const char kSubsystemName[];
-	static std::shared_ptr<Vision> self;
-	Vision();
+  static const char kSubsystemName[];
+  static std::shared_ptr<Vision> self;
+  Vision();
 
 public:
-	static std::shared_ptr<Vision> getInstance();
+  static std::shared_ptr<Vision> getInstance();
 
-	void InitDefaultCommand();
+  void InitDefaultCommand();
 };
 
 #endif  // Vision_H
