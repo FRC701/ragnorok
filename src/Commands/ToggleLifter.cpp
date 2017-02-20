00df6cd10d138570c2a62ff449023949dbbaca25
duane701 <dmurphy@vandenrobotics.com>
2017-02-20 14:03:33 -0800

    ToggleLifter return false for IsFinishes

--- src/Commands/ToggleLifter.cpp
+++ src/Commands/ToggleLifter.cpp
@@ -34,7 +34,7 @@ void ToggleLifter::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool ToggleLifter::IsFinished() {
-	return true;
+	return false;
 }
 //
 // Called once after isFinished returns true
