6fdfa2e1b10da843430b4484673b2df08b5f2519
SkylerPIlot <SkylerParthemore@gmail.com>
2017-02-20 14:38:46 -0800

    Added Subsystem for Lights

---
+++ src/Subsystems/Lights.h
@@ -0,0 +1,24 @@
+#ifndef Lights_H
+#define Lights_H
+
+#include <Commands/Subsystem.h>
+#include <Relay.h>
+
+class Lights : public frc::Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+
+  static const char kSubsystemName[];
+  static std::shared_ptr<Lights> self;
+  Lights();
+
+  Relay glow;
+public:
+
+  static std::shared_ptr<Lights> getInstance();
+
+	void InitDefaultCommand();
+};
+
+#endif  // Lights_H
