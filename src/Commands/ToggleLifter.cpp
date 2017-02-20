7707d326240a9fe0b1d15f0a2cda864bcc350d4a
MajorDarius <majorminor777@hotmail.com>
2017-02-20 13:11:13 -0800

    Added ToggleLifter command and buttons for driver and coDriver.

---
+++ src/Commands/ToggleLifter.cpp
@@ -0,0 +1,49 @@
+#include "ToggleLifter.h"
+#include "../Subsystems/Lifter.h"
+
+ToggleLifter::ToggleLifter(double speed) :
+		mSpeed(speed), turnOn(true)	{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Lifter::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void ToggleLifter::Initialize() {
+  Lifter::getInstance()->SetBrake();
+
+  double current = Lifter::getInstance()->GetCurrent();
+  if(current <= 0) {
+	  turnOn = true;
+  }
+  else if (current > 0) {
+	  turnOn = false;
+  }
+}
+
+// Called repeatedly when this Command is scheduled to run
+void ToggleLifter::Execute() {
+  if(turnOn) {
+	  Lifter::getInstance()->SetLifter(mSpeed);
+  }
+  else if (!turnOn) {
+	  Lifter::getInstance()->SetLifter(0.0);
+  }
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool ToggleLifter::IsFinished() {
+	return true;
+}
+//
+// Called once after isFinished returns true
+void ToggleLifter::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void ToggleLifter::Interrupted() {
+
+}
---
+++ src/Commands/ToggleLifter.h
@@ -0,0 +1,19 @@
+#ifndef ToggleLifter_H
+#define ToggleLifter_H
+
+#include "Commands/Command.h"
+
+class ToggleLifter : public frc::Command {
+public:
+	ToggleLifter(double speed);
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+private:
+	double mSpeed;
+	bool turnOn;
+};
+
+#endif  // ToggleLifter_H
