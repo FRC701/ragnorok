9b39ed6b71accc692fdfb494bae5a3f6ec66a5f1
OliviaRS <writingbloc99@gmail.com>
2017-03-19 14:57:31 -0700

    adjustments for driving
    -gear roller now runs in reverse while scoring for a second
    -intake roller now runs 2*peak power while shooting

--- src/Commands/GearScore.cpp
+++ src/Commands/GearScore.cpp
@@ -17,7 +17,8 @@ GearScore::GearScore() {
   AddParallel(new SetGearRoller(-RobotMap::kPeakPower));
   //AddSequential(new Delay(250));
   AddSequential(new SetGear(GearPickup::kGearDown));
-  //AddSequential(new Delay(.25)); //TODO: motion profile?
+  AddParallel(new Delay(1)); //TODO: motion profile?
+  AddSequential(new SetGearRoller(-RobotMap::kPeakPower));
   AddSequential(new GearQuit());
 
 	// To run multiple commands at the same time,
