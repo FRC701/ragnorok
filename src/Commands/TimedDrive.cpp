78d8a138af8000f1f2486cb30244691195337a3d
OliviaRS <writingbloc99@gmail.com>
2017-03-20 16:00:10 -0700

    created a timed drive command and added it to the retreat of center gear auto

---
+++ src/Commands/TimedDrive.cpp
@@ -0,0 +1,30 @@
+#include "TimedDrive.h"
+#include "Subsystems/Chassis.h"
+
+TimedDrive::TimedDrive(double timeout, double leftSpeed,double rightSpeed) : TimedCommand(timeout),
+mLeftSpeed(leftSpeed), mRightSpeed(rightSpeed) {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+  Requires(Chassis::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void TimedDrive::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TimedDrive::Execute() {
+  Chassis::getInstance()->SetTankDrive(mLeftSpeed, mRightSpeed);
+}
+
+// Called once after command times out
+void TimedDrive::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void TimedDrive::Interrupted() {
+
+}
