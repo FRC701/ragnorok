0a4170c5e6b405fb5e709afb87146a6c49f748dd
Mr. Murphy <dmurphy@vandenrobotics.com>
2017-03-21 22:19:29 -0700

    fixup! Use the SendableChooser

---
+++ src/Commands/AutoLeftGearDispatch.h
@@ -0,0 +1,11 @@
+#ifndef AutoLeftGearDispatch_H
+#define AutoLeftGearDispatch_H
+
+#include <Commands/CommandGroup.h>
+
+class AutoLeftGearDispatch : public CommandGroup {
+public:
+	AutoLeftGearDispatch();
+};
+
+#endif  // AutoLeftGearDispatch_H
---
+++ src/Commands/AutoRightGearDispatch.h
@@ -0,0 +1,11 @@
+#ifndef AutoRightGearDispatch_H
+#define AutoRightGearDispatch_H
+
+#include <Commands/CommandGroup.h>
+
+class AutoRightGearDispatch : public CommandGroup {
+public:
+	AutoRightGearDispatch();
+};
+
+#endif  // AutoRightGearDispatch_H
