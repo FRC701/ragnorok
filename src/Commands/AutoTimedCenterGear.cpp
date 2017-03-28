74632384e57e8e17a76ea1ef315a1a1be1db5d50
Mr. Murphy <dmurphy@vandenrobotics.com>
2017-03-27 18:08:30 -0700

    Add AutoTimedCenterGear

---
+++ src/Commands/AutoTimedCenterGear.cpp
@@ -0,0 +1,44 @@
+#include "AutoTimedCenterGear.h"
+#include "AutoLeftGear.h"
+#include "AutoDrive.h"
+#include "GearScore.h"
+#include "SetShifter.h"
+#include "SetAutoShifter.h"
+#include "Delay.h"
+#include "SetGearRoller.h"
+#include "../RobotMap.h"
+#include "SetSqueeze.h"
+#include "SetGear.h"
+#include "TimedDrive.h"
+#include "AutoTurn.h"
+
+AutoTimedCenterGear::AutoTimedCenterGear() {
+
+  // Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+  AddSequential(new SetAutoShifter(Chassis::kAutoShiftOff));
+  AddSequential(new SetShifter(Chassis::kShifterLow));
+  AddSequential(new TimedDrive(1.5,-.8,-.8));
+  /*AddSequential(new SetSqueeze(GearPickup::kSqueezeOpen));
+  AddParallel(new Delay(1));
+  AddSequential(new SetGear(GearPickup::kGearDown));
+  //AddParallel(new GearScore());
+  AddSequential(new SetGearRoller(-RobotMap::kPeakPower));
+  AddSequential(new SetGear(GearPickup::kGearDown));
+  AddParallel(new TimedDrive(1,.5,.5));
+  AddSequential(new SetGearRoller(-RobotMap::kPeakPower));
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.*/
+
+}
